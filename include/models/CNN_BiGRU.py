7979d1b03cc92f010dc47ee40216f3fba5cada21
AdiKsOnDev <adilalizade13@gmail.com>
2025-02-24 16:04:05 +0400

    refactor: Made a function that returns the compiled model

--- include/models/CNN_BiGRU.py
+++ include/models/CNN_BiGRU.py
@@ -4,23 +4,29 @@ from keras import layers, models, losses
 
 load_dotenv()
 
-VOCABULARY_SIZE = os.getenv("DLL_VOCABULARY_SIZE")
-CNN_BiGRU = models.Sequential()
+VOCABULARY_SIZE = int(os.getenv("DLL_VOCABULARY_SIZE"))
 
-CNN_BiGRU.add(layers.Embedding(VOCABULARY_SIZE, 10))
 
-# Convolutional Layer
-CNN_BiGRU.add(layers.Conv1D(128, (4, 4), activation='relu'))
-CNN_BiGRU.add(layers.MaxPooling1D((2, 2)))
-CNN_BiGRU.add(layers.Dropout(0.2))
+def compile():
+    CNN_BiGRU = models.Sequential()
 
-# BiGRU
-CNN_BiGRU.add(layers.Bidirectional(layers.GRU(3)))
+    CNN_BiGRU.add(layers.Embedding(VOCABULARY_SIZE, 10))
 
-# Fully Connected Layer
-CNN_BiGRU.add(layers.Dense(25))
-CNN_BiGRU.add(layers.Dropout(0.5))
-CNN_BiGRU.add(layers.Dense(25))
-CNN_BiGRU.add(layers.Dropout(0.5))
+    # Convolutional Layer
+    CNN_BiGRU.add(layers.Conv1D(128, 4, activation='relu'))
+    CNN_BiGRU.add(layers.MaxPooling1D(2))
+    CNN_BiGRU.add(layers.Dropout(0.2))
 
-CNN_BiGRU.compile(loss=losses.BinaryCrossentropy(), optimizer='adam')
+    # BiGRU
+    CNN_BiGRU.add(layers.Bidirectional(layers.GRU(3)))
+
+    # Fully Connected Layer
+    CNN_BiGRU.add(layers.Dense(25))
+    CNN_BiGRU.add(layers.Dropout(0.5))
+    CNN_BiGRU.add(layers.Dense(25))
+    CNN_BiGRU.add(layers.Dropout(0.5))
+    CNN_BiGRU.add(layers.Dense(2, activation='sigmoid'))
+
+    CNN_BiGRU.compile(loss=losses.BinaryCrossentropy(), optimizer='adam')
+
+    return CNN_BiGRU
