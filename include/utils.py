5de8ed26a6ed87bf22de315f7256d058aebabb77
AdiKsOnDev <adilalizade13@gmail.com>
2025-02-26 22:38:01 +0400

    refactor: Moved code to run CNN_BiGRU into utils

--- include/utils.py
+++ include/utils.py
@@ -1,32 +1,37 @@
+import os
 import numpy as np
 import tensorflow as tf
 from gensim.models import Word2Vec
+from keras.models import load_model
 from nltk.tokenize import NLTKWordTokenizer
+from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 def tokenize(string):
     return NLTKWordTokenizer.tokenize(string)
 
-def preprocess_functions_column(df, tokenizer):
+def preprocess_for_CNN_BiGRU(df, tokenizer, feature):
     """
     Preprocesses and divides the dataframe,
-    returning X, y for Functions
+    returning X, y for the given feature
 
     Args:
-        - df (pd.Dataframe): Data
-        - tokenizer (nltk.tokenize.NLTKWordTokenizer): Tokenizer to be used
+        df (pd.Dataframe): Data
+        tokenizer (nltk.tokenize.NLTKWordTokenizer): Tokenizer to be used
+        feature (str): Feature that has to be encoded and vectorized
 
     Returns:
-        - X: Vector representations of the Functions
-        - y: Encoded labels for each Function combination
+        X: Vector representations of the Functions
+        y: Encoded labels for each Function combination
     """
-    df_dlls = df[["Functions", "Label"]].copy()
-    df_dlls = df_dlls[df_dlls["Functions"].notna()]
-    api_calls = df_dlls['Functions']
+    df_dlls = df[[feature, "Label"]].copy()
+    df_dlls = df_dlls[df_dlls[feature].notna()]
+    api_calls = df_dlls[feature]
 
     w2v_model = Word2Vec(sentences=api_calls, vector_size=100,
                          window=5, min_count=1, workers=4)
-    tokenizer.fit_on_texts(df_dlls['Functions'])
+    tokenizer.fit_on_texts(df_dlls[feature])
     word_index = tokenizer.word_index
     embedding_dim = 100
 
@@ -40,7 +45,7 @@ def preprocess_functions_column(df, tokenizer):
             # Random for unknown words
             embedding_matrix[i] = np.random.uniform(-0.01, 0.01, embedding_dim)
 
-    sequences = tokenizer.texts_to_sequences(df_dlls['Functions'])
+    sequences = tokenizer.texts_to_sequences(df_dlls[feature])
     max_length = max(len(seq) for seq in sequences)
     labels = df_dlls['Label'].astype('category').cat.codes
 
@@ -49,3 +54,43 @@ def preprocess_functions_column(df, tokenizer):
         labels, num_classes=len(df_dlls['Label'].unique()))
 
     return X, y
+
+def run_CNN_BiGRU(df, tokenizer, feature='Sections', model_path="./models"):
+    df_api = df[[feature, 'Label']].copy()
+    df_api = df_api[df_api[feature].notna()]
+
+    train_api, test_api = train_test_split(
+        df_api,
+        test_size=0.25,
+        random_state=42,
+        stratify=df_api['Label']
+    )
+
+    if not os.path.exists(f'{model_path}/CNN_BiGRU_{feature}.h5'):
+        CNN_BiGRU = compile()
+        X, y = preprocess_for_CNN_BiGRU(train_api, tokenizer, feature)
+
+        CNN_BiGRU.fit(x=X, y=y, batch_size=32, epochs=16, verbose=2)
+        CNN_BiGRU.save(f'{model_path}/CNN_BiGRU_Sections.h5')
+    else:
+        CNN_BiGRU = load_model(f'{model_path}/CNN_BiGRU_Sections.h5')
+
+    X, y = preprocess_for_CNN_BiGRU(test_api, tokenizer, feature)
+
+    y_hat = CNN_BiGRU.predict(X)
+    y_hat = (y_hat > 0.5).astype(int)  # Classify the raw probabilities
+
+    accuracy = accuracy_score(y_hat, y)
+    precision = precision_score(y_hat, y, average="weighted")
+    recall = recall_score(y_hat, y, average="weighted")
+    f1 = f1_score(y_hat, y, average="weighted")
+
+    metrics = {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1_score": f1,
+    }
+
+    for metric, value in metrics.items():
+        print(f"{metric}: {value:.5f}")
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
+import sys
 import pandas as pd
 from dotenv import load_dotenv
-from keras.models import load_model
-from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 from include.models.CNN_BiGRU import compile
-from include.utils import preprocess_functions_column
+from include.utils import preprocess_for_CNN_BiGRU, run_CNN_BiGRU
+
+if len(sys.argv) == 1:
+    print("Please choose which experiment to run, options:")
+    print("\tcnnbigru-functions")
+    print("\tcnnbigru-dlls")
+
+    exit()
 
 DATASET_PATH = os.getenv('DATASET_PATH')
 RESULTS_PATH = os.getenv('RESULTS_PATH')
@@ -16,41 +21,9 @@ load_dotenv()
 df = pd.read_csv(DATASET_PATH)
 tokenizer = Tokenizer()
 
-df_api = df[['Functions', 'Label']].copy()
-df_api = df_api[df_api['Functions'].notna()]
-
-train_api, test_api = train_test_split(
-    df_api,
-    test_size=0.25,
-    random_state=42,
-    stratify=df_api['Label']
-)
-
-if not os.path.exists(f'{RESULTS_PATH}/CNN_BiGRU.h5'):
-    CNN_BiGRU = compile()
-    X, y = preprocess_functions_column(train_api, tokenizer)
-
-    CNN_BiGRU.fit(x=X, y=y, batch_size=32, epochs=16, verbose=2)
-    CNN_BiGRU.save(f'{RESULTS_PATH}/CNN_BiGRU.h5')
-else:
-    CNN_BiGRU = load_model(f'{RESULTS_PATH}/CNN_BiGRU.h5')
-
-X, y = preprocess_functions_column(test_api, tokenizer)
-
-y_hat = CNN_BiGRU.predict(X)
-y_hat = (y_hat > 0.5).astype(int)  # Classify the raw probabilities
-
-accuracy = accuracy_score(y_hat, y)
-precision = precision_score(y_hat, y, average="weighted")
-recall = recall_score(y_hat, y, average="weighted")
-f1 = f1_score(y_hat, y, average="weighted")
-
-metrics = {
-    "accuracy": accuracy,
-    "precision": precision,
-    "recall": recall,
-    "f1_score": f1,
-}
-
-for metric, value in metrics.items():
-    print(f"{metric}: {value:.4f}")
+if sys.argv[1] == "cnnbigru-functions":
+    run_CNN_BiGRU(df, tokenizer, 'Functions', RESULTS_PATH)
+elif sys.argv[1] == "cnnbigru-dlls":
+    run_CNN_BiGRU(df, tokenizer, 'DLLs', RESULTS_PATH)
+elif sys.argv[1] == "cnnbigru-sections":
+    run_CNN_BiGRU(df, tokenizer, 'Sections', RESULTS_PATH)
