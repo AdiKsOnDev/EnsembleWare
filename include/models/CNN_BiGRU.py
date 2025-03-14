fabb955ed7659e4807768373fb7509740e5813db
AdiKsOnDev <adilalizade13@gmail.com>
2025-03-14 05:53:07 +0400

    refactor: Moved model training function to CNN BiGRU file

--- include/models/CNN_BiGRU.py
+++ include/models/CNN_BiGRU.py
@@ -1,6 +1,12 @@
+import os
 import logging
 from dotenv import load_dotenv
+from keras.models import load_model
 from keras import layers, models, losses
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+from include.utils import preprocess_textual
 
 load_dotenv()
 logger = logging.getLogger('models')
@@ -35,3 +41,58 @@ def compile_model(VOCABULARY_SIZE):
     logger.info('Model compiled, returning')
 
     return CNN_BiGRU
+
+
+def run_CNN_BiGRU(df, tokenizer, feature='Sections', model_path="./models"):
+    """
+    Preprocesses the dataframe, then compiles, trains,
+    and evaluates the CNN BiGRU model
+
+    Args:
+        df (pd.DataFrame): Dataset
+        tokenizer (keras.preprocessing.text.Tokenizer): Keras tokenizer
+        feature (str): Column to be used for model fitting
+        model_path (str): Path where the function will save and/or look for the model
+    """
+    df_api = df[[feature, 'Label']].copy()
+    df_api = df_api[df_api[feature].notna()]
+
+    train_api, test_api = train_test_split(
+        df_api,
+        test_size=0.25,
+        random_state=42,
+        stratify=df_api['Label']
+    )
+    logger.debug(f'Train set size is {len(train_api)}, while Test set size is {len(test_api)}')
+
+    if not os.path.exists(f'{model_path}/CNN_BiGRU_{feature}.h5'):
+        logger.warning(f'Model wasn\'t found in {model_path}/CNN_BiGRU_{feature}.h5, starting model compilation')
+        X, y, vocabulary_size = preprocess_textual(train_api, tokenizer, feature)
+        CNN_BiGRU = compile_model(vocabulary_size)
+
+        CNN_BiGRU.fit(x=X, y=y, batch_size=32, epochs=16, verbose=2)
+        CNN_BiGRU.save(f'{model_path}/CNN_BiGRU_{feature}.h5')
+        logger.info(f'Model saved in {model_path}/CNN_BiGRU_{feature}.h5')
+    else:
+        CNN_BiGRU = load_model(f'{model_path}/CNN_BiGRU_{feature}.h5')
+        logger.warning(f'Model loaded from {model_path}/CNN_BiGRU_{feature}.h5')
+
+    X, y, _ = preprocess_textual(test_api, tokenizer, feature)
+
+    y_hat = CNN_BiGRU.predict(X)
+    y_hat = (y_hat > 0.5).astype(int)  # Classify the raw probabilities
+
+    accuracy = accuracy_score(y_hat, y)
+    precision = precision_score(y_hat, y)
+    recall = recall_score(y_hat, y)
+    f1 = f1_score(y_hat, y)
+
+    metrics = {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1_score": f1,
+    }
+
+    for metric, value in metrics.items():
+        print(f"{metric}: {value:.5f}")
