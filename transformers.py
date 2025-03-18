3dab18060d40a0de5c3ac4abc235e13cdb8f1aa7
AdiKsOnDev <adilalizade13@gmail.com>
2025-03-18 06:34:14 +0400

    feat: Fine-tuned the transformer models

---
+++ transformers.py
@@ -0,0 +1,49 @@
+import os
+import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tqdm import tqdm
+
+from include.Dataset import Dataset
+from include.trainer import fine_tune
+from include.utils import setup_loggers
+from include.model.transformers.bert import BERTModel
+from include.model.transformers.roformer import RoformerModel
+
+df = pd.read_csv('./data/PE_Dataset_Labeled.csv')
+
+setup_loggers(log_level=logging.DEBUG)
+main_logger = logging.getLogger('main')
+
+models = [
+        RoformerModel(model_name="junnyu/roformer_chinese_base", 
+                      num_labels=2),
+        BERTModel(model_name="bert-base-uncased", 
+                  num_labels=2)
+    ]
+features = ["DLLs", "Functions", "Sections"]
+
+main_logger.info("Starting the cycle without preprocessing")
+for feature in features:
+    for model in models:
+        main_logger.debug(f"Started the pipeline for {model.model_name} using {feature} Names")
+
+        df["Label"] = label_encoder.fit_transform(df["Label"])
+
+        texts = df[feature].tolist()
+        labels = df["label"].tolist()
+
+        train_X, test_X, train_y, test_y = train_test_split(
+            texts, labels, test_size=0.25, random_state=42, stratify=labels
+        )
+
+        main_logger.info(f"Tokenizing for {model.model_name}")
+
+        train_X = model.tokenize(train_X)
+        test_X = model.tokenize(test_X)
+        train_dataset = Dataset(train_X, train_y)
+        test_dataset = Dataset(test_X, test_y)
+
+        main_logger.debug(f"About to start fine-tuning {model.model_name}")
+        fine_tune(model, train_dataset, test_dataset)
